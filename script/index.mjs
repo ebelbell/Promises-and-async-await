226a50ce7e46822adb0576241fabc565459d3aee
ebelbell <ebella2056@gmail.com>
2024-05-28 15:56:25 -0700

    added some code from the assignment doc

--- script/index.mjs
+++ script/index.mjs
@@ -4,12 +4,74 @@ Use async/await syntax to gather data from asynchronous sources.
 Use Promises to handle asynchronous code. */
 
 // Importing database functions. DO NOT MODIFY THIS LINE.
-import { central, db1, db2, db3, vault } from "./databases.js";
+// the central database identifies which database the users are stored within
+import { central, db1, db2, db3, vault } from "./databases.mjs";
 
-function getUserData(id) {
+function getUserData(id) { //simple dictionary object
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
 }
+
+/***********SCENARIO #1: 
+You are a developer in a very large corporation that splits its data across multiple databases. 
+Your job is to assemble this information using a single function that takes an id parameter and returns a Promise that 
+resolves to an object containing specific data. The object must contain the following information, 
+which will be gathered from the databases: ****************/
+
+
+// create a function that takes an id parameter 
+// and returns a Promise that resolves to an object containing specific data. 
+function resolveObjectData(resolve) {
+  const data = {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: {
+      street: string,
+      suite: string,
+      city: string,
+      zipcode: string,
+      geo: {
+        lat: string,
+        lng: string
+      }
+    },
+    phone: string,
+    website: string,
+    company: {
+      name: string,
+      catchPhrase: string,
+      bs: string
+    }
+  }
+  resolve(data);
+}
+
+//the central database returns a string that identifies which database to access the user's info
+const returnedValue = await central(id);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//access the central database
+const returnedValue = await central (id);
+
+returnedValue.then((data) => {
+  console.log(`${data} has been accessed`); //return a string message if the data passes the returnedValue function
+});
